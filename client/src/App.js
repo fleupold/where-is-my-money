55871f8038aaa99b8617cfb6f3d742449227e178
Felix Leupold <felixleupold90@gmail.com>
2019-04-08 14:51:41 +1000

    init client

---
+++ client/src/App.js
@@ -0,0 +1,9 @@
+import React, { Component } from "react";
+
+class App extends Component {
+  render() {
+    return <div>I'M READY TO USE THE BACK END APIS! :-)</div>;
+  }
+}
+
+export default App;
