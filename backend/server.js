fda158856d5b493e4cdcb5a438c4e764730069c5
Felix Leupold <felixleupold90@gmail.com>
2019-04-09 18:12:27 +1000

    using env port

--- backend/server.js
+++ backend/server.js
@@ -5,7 +5,7 @@ const bodyParser = require("body-parser");
 const logger = require("morgan");
 const Snippet = require("./data");
 
-const API_PORT = 3001;
+const API_PORT = process.env.PORT || 3001;
 const app = express();
 app.use(cors());
 const router = express.Router();
